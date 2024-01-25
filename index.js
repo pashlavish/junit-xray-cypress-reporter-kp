309d425d3767a593f7bc4c4d5cd3245aa11ded6d
pashlavish <54575055+pashlavish@users.noreply.github.com>
2024-01-25 15:40:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const debug = require('debug')('junit-xray-reporter')
 const MochaJUnitReporter = require('../mocha-junit-reporter/index.js')
 
-// get to it
-
 class JUnitXrayReporter extends MochaJUnitReporter {
 	constructor(runner, options) {
 		super(runner, options)
@@ -13,9 +11,17 @@ class JUnitXrayReporter extends MochaJUnitReporter {
 	getTestcaseData(test, err) {
 		const testcase = super.getTestcaseData(test, err)
 		const jiraID = this.generateJiraID(test)
+		const testData = this.getTestDataFromSpec(test)
 
 		if (jiraID.length) {
 			let property = [{ property: { _attr: { name: 'test_key', value: jiraID } } }]
+			testcase.testcase.push({
+				properties: property,
+			})
+		}
+
+		if (testData.length) {
+			let property = [{ property: { _attr: { name: 'testrun_comment', value: testData } } }]
 			testcase.testcase.push({
 				properties: property,
 			})
@@ -56,6 +62,41 @@ class JUnitXrayReporter extends MochaJUnitReporter {
 			return []
 		}
 		return jiraID
+	}
+
+	getTestDataFromSpec(element) {
+		const testConfig = element._testConfig
+		if (!testConfig) {
+			return []
+		}
+
+		//
+		const outdatedConfig = testConfig['env'] ? testConfig.env : testConfig.unverifiedTestConfig?.env
+		if (outdatedConfig) {
+			console.log(
+				"junit-xray-cypress-reporter v0.1.1 released: REPORTER OUTDATED CONFIG! Change the param 'env' with the value 'xray' to align the module with NEWEST CONFIGURATIONS"
+			)
+			if (!outdatedConfig) {
+				return []
+			}
+			const comments = outdatedConfig['runComments']
+
+			if (!comments) {
+				return []
+			}
+			return comments
+		}
+
+		const envConfig = testConfig['xray'] ? testConfig.xray : testConfig.unverifiedTestConfig?.xray
+		if (!envConfig) {
+			return []
+		}
+		const comments = envConfig['runComments']
+
+		if (!comments) {
+			return []
+		}
+		return comments
 	}
 }
 
